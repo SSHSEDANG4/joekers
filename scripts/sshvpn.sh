1fdf63d1410c8ef6db2151fe22e9aa990d7db507
Doctype <87948372+cybertize@users.noreply.github.com>
2021-08-25 21:03:18 +0800

    Update sshvpn.sh

--- scripts/sshvpn.sh
+++ scripts/sshvpn.sh
@@ -94,10 +94,6 @@ apt -y -qq remove --purge apache2*
 apt -y -qq remove --purge bind9*
 apt -y -qq remove --purge sendmail*
 
-# setting port ssh
-sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
-sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
-
 # install nginx
 cd; apt -y install nginx
 rm /etc/nginx/sites-enabled/default
