cf19fad3ee355f6ff50ca0640f632a8cf506adb8
Doctype <87948372+cybertize@users.noreply.github.com>
2021-08-25 09:45:42 +0800

    Add files via upload

---
+++ plugins/server/about.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+
+echo -e "===============================================" | lolcat
+echo -e "               JoekersVPN                      " | lolcat
+echo -e "-----------------------------------------------" | lolcat
+echo -e " >> Debian 9 & Debian 10 64 bit               " | lolcat
+echo -e " >> Ubuntu 18.04 & Ubuntu 20.04 64 bit        " | lolcat
+echo -e "===============================================" | lolcat
---
+++ plugins/server/info.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+
+cat /root/log-install.txt
